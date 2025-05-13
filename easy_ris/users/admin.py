273337d563c32b68c68e020e26e0e0ad3a028837
tubo <tubo@tubo.nz>
2025-05-13 23:42:44 +1200

    Refactor UserAdmin and forms to streamline admin interface; enhance fieldsets and search functionality

--- easy_ris/users/admin.py
+++ easy_ris/users/admin.py
@@ -1,22 +1,14 @@
 from allauth.account.decorators import secure_admin_login
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
-from django.utils.translation import gettext_lazy as _
-
-from .forms import UserAdminChangeForm
-from .forms import UserAdminCreationForm
-from .models import User
-
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
-
-from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from unfold.admin import ModelAdmin
+from unfold.forms import AdminPasswordChangeForm
 
-
+from .forms import UserAdminChangeForm, UserAdminCreationForm
+from .models import User
 
 if settings.DJANGO_ADMIN_FORCE_ALLAUTH:
     # Force the `admin` sign in process to go through the `django-allauth` workflow:
@@ -30,10 +22,29 @@ admin.site.unregister(Group)
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, ModelAdmin):
-
-    form = UserChangeForm
-    add_form = UserCreationForm
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("name", "email")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ["username", "name", "is_superuser"]
+    search_fields = ["name"]
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
     change_password_form = AdminPasswordChangeForm
+
 
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
--- easy_ris/users/forms.py
+++ easy_ris/users/forms.py
@@ -2,22 +2,23 @@ from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 from django.contrib.auth import forms as admin_forms
 from django.utils.translation import gettext_lazy as _
+from unfold.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
 
-class UserAdminChangeForm(admin_forms.UserChangeForm):
-    class Meta(admin_forms.UserChangeForm.Meta):  # type: ignore[name-defined]
+class UserAdminChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):  # type: ignore[name-defined]
         model = User
 
 
-class UserAdminCreationForm(admin_forms.AdminUserCreationForm):
+class UserAdminCreationForm(UserCreationForm):
     """
     Form for User Creation in the Admin Area.
     To change user signup, see UserSignupForm and UserSocialSignupForm.
     """
 
-    class Meta(admin_forms.UserCreationForm.Meta):  # type: ignore[name-defined]
+    class Meta(UserCreationForm.Meta):  # type: ignore[name-defined]
         model = User
         error_messages = {
             "username": {"unique": _("This username has already been taken.")},
